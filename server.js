22e241a308a04edc59517ec0d553d9545d97c33b
Jay <jevonte_n@outlook.com>
2018-11-06 23:23:58 -0500

    port 300

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
 var express = require("express");
 var mongojs = require("mongojs");
 
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+
 var app = express();
+
+app.listen(3000, function() {
+    console.log("App running on port 3000!");
+  });
